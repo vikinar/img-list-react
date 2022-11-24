57a58d1df8de005a8a7b44c346794f978c9272a3
Narek <gnaro93@gmail.com>
2022-11-25 01:29:16 +0400

    Switch to Context Api

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useContext} from "react";
+import React, {useContext} from "react";
 import styles from "./search.module.scss"
 import {SearchContext} from "../CartList";
 
