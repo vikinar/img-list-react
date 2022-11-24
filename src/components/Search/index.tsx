b18e884579caa65ce7cfd362d687c948f4537d51
Narek <gnaro93@gmail.com>
2022-11-25 01:28:10 +0400

    Switch to Context Api

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,15 +1,13 @@
-import React, {useState, createContext, useCallback, useContext} from "react";
+import React, {useCallback, useContext} from "react";
 import styles from "./search.module.scss"
 import {SearchContext} from "../CartList";
-
-
 
 const Search:React.FC = () => {
     const {value, setValue} = useContext(SearchContext)
 
-    const changeValue = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const changeValue =(e: React.ChangeEvent<HTMLInputElement>) => {
         setValue(e.target.value)
-    }, [SearchContext])
+    }
 
     return (
         <section className={styles.search}>
